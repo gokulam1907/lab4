e8358c984aad2cf92101d1cf7d9bea372c89aa1c
gokulam1907 <2021wb86355@wilp.bits-pilani.ac.in>
2025-07-14 22:27:14 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,5 +1,7 @@
+package example;
+ 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, DevOps Lab!");
+        System.out.println("Hello, DevOps Lab - CI Test!");
     }
 }
